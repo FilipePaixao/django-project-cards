9d4762571d91cf07d42c50f66a135f216c43c811
Filipe Paixão <fpedrapaixao@gmail.com>
2025-07-03 16:03:10 -0300

    feat: Add images field to Item model and update ItemForm fields

--- Cards/models.py
+++ Cards/models.py
@@ -5,15 +5,16 @@ class Item(models.Model):
     description = models.TextField()
     quantity = models.PositiveIntegerField() 
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(max_length=250, blank=True, null=True)
 
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'quantity', 'price', 'description']
+        fields = ['name', 'quantity', 'price', 'description', 'images']
         
     def __str__(self):
         return self.name
-    
+        
     
     def total_price(self):
         return self.quantity * self.price
